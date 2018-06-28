531bce2382f69575186d28dfaecf31cc7c49f548
Kazuki Nishida <kazuki.nnn@gmail.com>
2018-06-28 22:53:22 +0900

    add an implementation to find duplicate strings

--- plugin/src/main/kotlin/com/github/kazkn/gradle/deduplication/StringsDeduplicationPlugin.kt
+++ plugin/src/main/kotlin/com/github/kazkn/gradle/deduplication/StringsDeduplicationPlugin.kt
@@ -3,9 +3,57 @@ package com.github.kazkn.gradle.deduplication
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jdom2.input.SAXBuilder
+import java.io.File
+
 
 class StringsDeduplicationPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        println("Hello" + project.name + "!")
+        project.task("deduplicationStrings").doLast {
+            // find res Dirs
+            val resDirs = project.rootProject.subprojects
+                    .map { it.projectDir.path + "/src/main/res" }
+                    .filter { File(it).exists() }
+
+            resDirs.flatMap { findStringsXmlPath(it) }
+                    .forEach {
+                        println("--- $it ---")
+                        checkStringsXml(it)
+                    }
+        }
+    }
+
+    // find values*/strings.xml
+    private fun findStringsXmlPath(resDirPath: String) = File(resDirPath).listFiles().flatMap {
+        if (it.isDirectory) {
+            it.listFiles().asIterable()
+        } else {
+            listOf(it)
+        }
+    }.filter {
+        it.path.matches(Regex("^.*values.*/strings.xml$"))
+    }
+
+    // find duplicate string
+    private fun checkStringsXml(xmlPath: File) {
+        val doc = SAXBuilder().build(xmlPath)
+
+        val map = HashMap<String, MutableList<String>>()
+
+        doc.rootElement.children.forEach { element ->
+            val value = element.value
+
+            if (map.containsKey(value)) {
+                map[value]?.add(element.getAttributeValue("name"))
+            } else {
+                map[value] = mutableListOf(element.getAttributeValue("name"))
+            }
+        }
+
+        map.entries
+                .filter { it.value.size >= 2 }
+                .forEach {
+                    println("\"${it.key}\" is defined ${it.value.size} times (${it.value.joinToString(",")})")
+                }
     }
 }
